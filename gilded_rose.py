b25a719b408c672dd5b6d5cf215224a973438327
Beth <shiehbeth@gmail.com>
2025-02-12 21:56:25 -0800

    code refactored w/ strategy pattern

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,6 +12,84 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
+class SubItem(Item):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    # def validate_quality(self, sell_in, quality):
+    #     return self.quality >= 0 and self.quality <= 50
+
+    def update_quality(self):
+        pass
+
+
+class NormalItem(SubItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in >= 0 and self.quality > 1:
+            self.quality -= 1
+        elif self.sell_in < 0 and self.quality > 2:
+            self.quality -= 2
+        else:
+            self.quality = 0
+        self.sell_in -= 1
+
+
+class AgedBrie(SubItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.quality < 50:
+            self.quality += 1
+        self.sell_in -= 1
+
+
+class Sulfuras(SubItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        self.sell_in -= 1
+
+
+class BackstagePasses(SubItem):
+    def __init__(self, name, sell_in, quality):
+        if quality < 0 or quality > 50:
+            raise ValueError("Quality must be between 0 and 50")
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in < 0:
+            self.quality = 0
+        elif self.sell_in <= 5:
+            if self.quality > 47 and self.quality <= 50:
+                self.quality = 50
+            elif self.quality <= 47:
+                self.quality += 3
+        elif self.sell_in <= 10:
+            if self.quality == 49 or self.quality == 50:
+                self.quality = 50
+            elif self.quality <= 48:
+                self.quality += 2
+
+
+class Conjured(SubItem):
+    def __init__(self, name, sell_in, quality):
+        super().__init__(name, sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in >= 0 and self.quality > 2:
+            self.quality -= 2
+        elif self.sell_in < 0 and self.quality > 4:
+            self.quality -= 4
+        else:
+            self.quality = 0
+        self.sell_in -= 1
+
+
 class GildedRose(object):
 
     def __init__(self, items: list[Item]):
@@ -19,31 +97,41 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+        for i in self.items:
+            i.update_quality()
+
+    def get_items(self):
+        return self.items
+
+    def add_item(self, SubItem):
+        self.items.append(SubItem)
+
+    # def update_quality_1(self):
+    #     for item in self.items:
+    #         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+    #             if item.quality > 0:
+    #                 if item.name != "Sulfuras, Hand of Ragnaros":
+    #                     item.quality = item.quality - 1
+    #         else:
+    #             if item.quality < 50:
+    #                 item.quality = item.quality + 1
+    #                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
+    #                     if item.sell_in < 11:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #                     if item.sell_in < 6:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #         if item.name != "Sulfuras, Hand of Ragnaros":
+    #             item.sell_in = item.sell_in - 1
+    #         if item.sell_in < 0:
+    #             if item.name != "Aged Brie":
+    #                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
+    #                     if item.quality > 0:
+    #                         if item.name != "Sulfuras, Hand of Ragnaros":
+    #                             item.quality = item.quality - 1
+    #                 else:
+    #                     item.quality = item.quality - item.quality
+    #             else:
+    #                 if item.quality < 50:
+    #                     item.quality = item.quality + 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -2,102 +2,100 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
+from gilded_rose import SubItem, AgedBrie, Sulfuras, BackstagePasses
+from gilded_rose import NormalItem, Conjured
 
 
 class GildedRoseTest(unittest.TestCase):
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [Sulfuras("Sulfuras", 5, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
-        self.assertEquals(80, sulfuras_item.quality)
-        self.assertEquals(4, sulfuras_item.sell_in)
-        self.assertEquals("Sulfuras", sulfuras_item.name)
+        self.assertEqual(50, sulfuras_item.quality)
+        self.assertEqual(4, sulfuras_item.sell_in)
+        self.assertEqual("Sulfuras", sulfuras_item.name)
 
     # example of test that checks for syntax errors
     def test_gilded_rose_list_all_items(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [Sulfuras("Sulfuras", 5, 50)]
         gilded_rose = GildedRose(items)
         all_items = gilded_rose.get_items()
-        self.assertEquals(["Sulfuras"], all_items)
+        self.assertEqual(items, all_items)
 
     # test logical error with invalid starting quality less than 0
     # should raise error
     def test_out_of_bounds_starting_quality_under(self):
-        items = [Item("Backstage passes", 1, -1)]
-        backstage_pass_item = items[0]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertRaises(ValueError, lambda: backstage_pass_item.quality)
+        with self.assertRaises(ValueError):
+            items = [BackstagePasses("Backstage passes", 1, -1)]
+            items.quality()
 
     # test logical error sell_in day of (technically not past sell_in day yet)
     # should not decrease quality by 2
     def test_boundary_sell_in_condition_quality_decrease_rate(self):
-        items = [Item("Banana", 0, 10)]
+        items = [NormalItem("Banana", 0, 10)]
         banana_item = items[0]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(9, banana_item.quality)
+        self.assertEqual(9, banana_item.quality)
 
     # test logical error of quality increase for Aged Brie behaves differently
     # at boundary condition
     def test_boundary_aged_brie_quality_increase_rate(self):
-        items = [Item("Aged Brie", 1, 15)]
+        items = [AgedBrie("Aged Brie", 1, 15)]
         aged_brie_item = items[0]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         # this correctly passes, and adds one to quality
-        self.assertEquals(16, aged_brie_item.quality)
+        self.assertEqual(16, aged_brie_item.quality)
         gilded_rose.update_quality()
         # this fails, since it adds two to quality, which is inconsistent
-        self.assertEquals(17, aged_brie_item.quality)
+        self.assertEqual(17, aged_brie_item.quality)
 
     # test syntax error for adding an item
     def test_gilded_rose_add_item(self):
-        items = [Item("Backstage passes", 1, 30)]
-        new_item = Item("Aged Brie", 5, 10)
+        items = [BackstagePasses("Backstage passes", 1, 30)]
+        new_item = AgedBrie("Aged Brie", 5, 10)
         gilded_rose = GildedRose(items)
         gilded_rose.add_item(new_item)
-        self.assertEquals(gilded_rose.items[1], new_item)
+        self.assertEqual(gilded_rose.items[1], new_item)
 
     # Additional failed tests implemented below:
 
     # test logical error with invalid starting quality over 50
     # for backstage passes should raise error
     def test_out_of_bounds_starting_quality_over(self):
-        items = [Item("Backstage passes", 1, 60)]
-        backstage_pass_item = items[0]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertRaises(ValueError, lambda: backstage_pass_item.quality)
+        with self.assertRaises(ValueError):
+            items = [BackstagePasses("Backstage passes", 1, 60)]
+            items.quality()
 
     # test error (incorrect naming) for backstage pass with <=10 sell
     # in days should increase quality by 2
     def test_backstage_pass_should_increase_quality_sell_in_leq_10(self):
-        items = [Item("Backstage passes", 8, 30)]
+        items = [BackstagePasses("Backstage passes", 8, 30)]
         backstage_pass_item = items[0]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(32, backstage_pass_item.quality)
+        self.assertEqual(32, backstage_pass_item.quality)
 
     # test error (incorrect naming) for backstage pass with <=3 sell
     # in days should increase quality by 3
     def test_backstage_pass_should_increase_quality_sell_in_leq_5(self):
-        items = [Item("Backstage passes", 3, 30)]
+        items = [BackstagePasses("Backstage passes", 3, 30)]
         backstage_pass_item = items[0]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(33, backstage_pass_item.quality)
+        self.assertEqual(33, backstage_pass_item.quality)
 
     # test error for backstage pass (incorrect naming) quality should
     # be 0 after concert date
     def test_backstage_pass_quality_drop_zero_after_concert_date(self):
-        items = [Item("Backstage passes", 0, 50)]
+        items = [BackstagePasses("Backstage passes", -1, 50)]
         backstage_pass_item = items[0]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals(0, backstage_pass_item.quality)
+        self.assertEqual(0, backstage_pass_item.quality)
 
 
 if __name__ == '__main__':
